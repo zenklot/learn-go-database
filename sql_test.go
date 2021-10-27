a029bdd493b68d36c89ab353f9284f9d12e5f263
zenklot <zenklot11@gmail.com>
2021-10-27 16:27:05 +0700

    writing code with sql query to select data

---
+++ sql_test.go
@@ -0,0 +1,46 @@
+package learn_golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES('Zez', 'Zez')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
+}
