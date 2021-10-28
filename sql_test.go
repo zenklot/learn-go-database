47591af8b389a6e28783609dd4ecc979e3e881fb
zenklot <zenklot11@gmail.com>
2021-10-28 12:13:15 +0700

    writing code with prepare statements for insert many

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -180,4 +181,34 @@ func TestExecSqlLastID(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println("Success insert new comment with id : ", insertId)
+}
+
+func TestExecSqlPrefarestatement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	statement, err := db.PrepareContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer statement.Close()
+
+	for i := 0; i < 10; i++ {
+		email := "email" + strconv.Itoa(i) + "2@email.com"
+		comment := "ini komentar"
+
+		result, err := statement.ExecContext(ctx, email, comment)
+		if err != nil {
+			panic(err)
+		}
+		insertId, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Success insert new comment with id : ", insertId)
+	}
+
 }
