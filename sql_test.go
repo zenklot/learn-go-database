b989c8ad4d28fdbd58b75189ea4405adad955363
zenklot <zenklot11@gmail.com>
2021-10-28 10:44:35 +0700

    select customer with many data type & Null

--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package learn_golang_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -27,7 +29,7 @@ func TestQuerySql(t *testing.T) {
 
 	ctx := context.Background()
 
-	script := "SELECT * FROM customer"
+	script := "SELECT id,name FROM customer"
 	rows, err := db.QueryContext(ctx, script)
 	if err != nil {
 		panic(err)
@@ -42,5 +44,47 @@ func TestQuerySql(t *testing.T) {
 		}
 		fmt.Println("Id:", id)
 		fmt.Println("Name:", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var birth_date sql.NullTime
+		var created_at time.Time
+		var married bool
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birth_date, &married, &created_at)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("========================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid {
+			fmt.Println("email:", email.String)
+		}
+
+		fmt.Println("balance:", balance)
+		fmt.Println("rating:", rating)
+		if birth_date.Valid {
+			fmt.Println("birth_date:", birth_date.Time)
+		}
+		fmt.Println("married:", married)
+		fmt.Println("created_at:", created_at)
 	}
 }
